85b4513e45076c816a7761cf7c6c137de9c1e111
lemuel-addy <98181554+lemuel-addy@users.noreply.github.com>
2023-09-04 01:31:20 +0000

    Time: 61 ms (87.51%), Space: 17.6 MB (50.84%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,8 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         # i = 0
-        ans = []
+        # ans = []
         # x = len(nums)
+
+
+        # sliding window with while loop
         # while i < x-1:
         #     j = i + 1
         #     while j < x:
@@ -13,12 +16,17 @@ class Solution:
         #     i+= 1
         # return ans
 
-
-        for i in range(0,len(nums)-1):
-            
-            for j in range(i+1,len(nums)):
-                
-                if nums[i] + nums[j] == target:
-                    ans.extend([i,j])
-        return ans
+        #sliding window with for loop
+        # for i in range(0,len(nums)-1):           
+        #     for j in range(i+1,len(nums)):               
+        #         if nums[i] + nums[j] == target:
+        #             ans.extend([i,j])
+        # return ans
         
+        # using a hashmap, finding if the complementary value exists
+        hmap = {}
+        for i,n in enumerate(nums):
+            diff = target - n
+            if diff in hmap:
+                return [hmap[diff],i]
+            hmap[n] = i
