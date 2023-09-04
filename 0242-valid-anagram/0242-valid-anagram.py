da766865ba2eadbe7fb1b9cf3bdeaf4145245177
lemuel-addy <98181554+lemuel-addy@users.noreply.github.com>
2023-09-04 01:06:20 +0000

    Time: 38 ms (98.94%), Space: 16.7 MB (86.38%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,5 +1,6 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        # time complexity of O(nLogn), space of O(n)
         # t = sorted(t)
         # s = sorted(s)
         # if t == s:
@@ -7,16 +8,19 @@ class Solution:
         # else:
         #     return False
 
-        if len(t) != len(s):
-            return False
-        smap = {}
-        tmap = {}
-        for i in range(len(s)):
-            smap[s[i]] = 1 + smap.get(s[i],0)
-            tmap[t[i]] = 1 + tmap.get(t[i],0)      
-        if smap == tmap:
-            return True
-        else:
-            return False
+        # time complexity of O(N) and space of O(N)
+        # if len(t) != len(s):
+        #     return False
+        # smap = {}
+        # tmap = {}
+        # for i in range(len(s)):
+        #     smap[s[i]] = 1 + smap.get(s[i],0)
+        #     tmap[t[i]] = 1 + tmap.get(t[i],0)      
+        # if smap == tmap:
+        #     return True
+        # else:
+        #     return False
+
+        return Counter(s) == Counter(t)
        
         
