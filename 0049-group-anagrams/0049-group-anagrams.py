b3ee238ba03e9559ff611316537d937b250ed022
lemuel-addy <98181554+lemuel-addy@users.noreply.github.com>
2023-09-05 00:29:28 +0000

    Time: 107 ms (52.91%), Space: 20.5 MB (45.98%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,25 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # ans = collections.defaultdict(list)
+
+        # for s in strs:
+        #     count = [0] * 26
+        #     for c in s:
+        #         count[ord(c) - ord("a")] += 1
+        #     ans[tuple(count)].append(s)
+        # return ans.values()
+
+        map = defaultdict(list)
+        
+        result = []
+        for s in strs:
+            sorted_s = tuple(sorted(s))
+            #tuple makes it able to be used as a key i.e. storing an array as a single variable
+            print (sorted_s)
+            #get the sorted form and use it as a key, hence all anagrams add to it
+            #appending the values themselves not adding the occurences like in 1 + map(s[i],0)
+            map[sorted_s].append(s)
+            
+        for value in map.values():
+            result.append(value)
+        return result
