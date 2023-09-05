0d2726377c3f49b3e34c4e88cf925abc56f245ed
lemuel-addy <98181554+lemuel-addy@users.noreply.github.com>
2023-09-05 01:17:05 +0000

    Time: 102 ms (62.13%), Space: 21.9 MB (30.34%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,21 +1,33 @@
 from collections import defaultdict
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        ar = defaultdict(list)
-        freq = []
+        # ar = defaultdict(list)
+        # freq = []
         
-        for i in nums:
-            ar[i].append(i)
-        for value in ar.values():
-            freq.append(value)
-        freq = sorted(freq, key=len,reverse=True)
+        # for i in nums:
+        #     ar[i].append(i)
+        # for value in ar.values():
+        #     freq.append(value)
+        # freq = sorted(freq, key=len,reverse=True)
+        # print (freq)
   
-        for i in range(0,len(freq)):
-            freq[i] = freq[i][0]
+        # for i in range(0,len(freq)):
+        #     freq[i] = freq[i][0]
            
-        return freq[:k]
+        # return freq[:k]
 
-
+        count={}
+        freq = [[] for i in range(len(nums)+1)]
+        for n in nums:
+            count[n] = 1 + count.get(n,0)
+        for n,c in count.items():
+            freq[c].append(n)
+        res = []
+        for i in range(len(freq)-1,0,-1):
+             for n in freq[i]:
+                 res.append(n)
+                 if len(res)==k:
+                     return res
 
 
         
