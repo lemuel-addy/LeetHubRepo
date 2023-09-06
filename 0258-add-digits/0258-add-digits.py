9d7d7013cb4ca095e7e9ad2805c800e935e39a85
lemuel-addy <98181554+lemuel-addy@users.noreply.github.com>
2023-09-06 22:37:40 +0000

    Time: 48 ms (23.68%), Space: 16.5 MB (21.61%) - LeetHub

--- 0258-add-digits/0258-add-digits.py
+++ 0258-add-digits/0258-add-digits.py
@@ -1,12 +1,21 @@
 class Solution:
     def addDigits(self, num: int) -> int:
-        if num < 10:
-            return num
+        # if num < 10:
+        #     return num
 
-        digits = []
-        while num:
+        # digits = []
+        # while num:
+        #     digit = num%10
+        #     num = num//10 
+        #     digits.append(digit)
+        #return self.addDigits(sum(digits))
+        #recursion is a O(N) space because the recursive calls use the core stack ecursion typically uses the call stack. When a function calls itself recursively, each new function call is pushed onto the call stack. This allows the program to keep track of the multiple instances of the function that are currently executing.
+       
+
+        while num >= 10:
             digit = num%10
-            num = num//10 
-            digits.append(digit)
-        return self.addDigits(sum(digits))
-
+            num = num//10 + digit
+            print(num//10)
+            print(digit)
+        return num
+            
