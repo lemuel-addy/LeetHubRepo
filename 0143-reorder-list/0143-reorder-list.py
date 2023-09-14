0b457ab52719ba183a82da4e4c94613e7bba1b1e
Lemuel Addy <Lemuel.addy@gmail.com>
2023-09-14 04:13:42 +0000

    Time: 83 ms (49.57%), Space: 26.2 MB (34.87%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -8,30 +8,47 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        # holder = ListNode()
-        # first = last = head
-        # while last.next is not None:
-        #     last = last.next
             
-        # while first < last:
-        #     holder = first.val
-        #     holder.next = last.val
-            
-        nodes = []
-        dummy = head
-        while dummy:
-            nodes.append(dummy)
-            dummy = dummy.next
+        # nodes = []
+        # dummy = head
+        # while dummy:
+        #     nodes.append(dummy)
+        #     dummy = dummy.next
 
-        n = len(nodes) if len(nodes) % 2 == 0 else len(nodes) + 1
-        i = 0
+        # n = len(nodes) if len(nodes) % 2 == 0 else len(nodes) + 1
+        # i = 0
+        # prev = None
+        # while i < (n // 2):
+        #     if prev: #prev != None
+        #         prev.next = nodes[i]
+
+        #     nodes[i].next = nodes[-1 - i]
+        #     prev = nodes[-1 - i]
+        #     i += 1
+
+        # prev.next = None
+
+
+        slow,fast = head, head.next
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        second = slow.next
+        slow.next = None
         prev = None
-        while i < (n // 2):
-            if prev: #prev != None
-                prev.next = nodes[i]
-
-            nodes[i].next = nodes[-1 - i]
-            prev = nodes[-1 - i]
-            i += 1
-
-        prev.next = None
+        while second:
+            hold = second.next
+            second.next = prev
+            prev = second
+            second = hold
+        
+        while prev:
+            hold = prev.next
+            hild = head.next
+            head.next = prev
+            prev.next = hild
+            head = hild
+            prev = hold
+            
