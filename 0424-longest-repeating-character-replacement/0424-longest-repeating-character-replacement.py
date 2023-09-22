b3157fe7cfe1aa012f67477b5df45f4887f4b9d8
Lemuel Addy <Lemuel.addy@gmail.com>
2023-09-22 00:10:15 +0000

    Time: 138 ms (28.18%), Space: 16.4 MB (69.59%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -0,0 +1,53 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        left = maxLen = 0 
+        hmap = {}
+        for r in range(0,len(s)):
+            hmap[s[r]] = hmap.get(s[r],0)+1
+            while(r-left+1)- max(hmap.values()) > k:
+                hmap[s[left]] -= 1
+                left += 1
+            maxLen = max(maxLen,r-left+1)
+        return maxLen
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # track = 1
+        # hold = 0
+        # rep = 0
+        # for i in range(0,len(s)-1):
+        #     j = i + 1
+        #     x = i
+        #     while rep < k and j < len(s):
+        #             if (s[j] != s[x]):
+        #                 rep += 1
+        #                 track +=1
+
+        #             else:
+        #                 track +=1
+        #             j += 1
+        #             x += 1
+            
+        #     rep = 0
+        #     hold = max(hold,track)
+        #     track = 1
+        # # hold = max(hold,track)
+        # return hold + 1
+
+
+                    
+
+                
+
