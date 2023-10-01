82283e539350896df3ecad8f67726afee832eb3d
Lemuel Addy <Lemuel.addy@gmail.com>
2023-10-01 22:33:33 +0000

    Time: 276 ms (12.63%), Space: 20.7 MB (51.58%) - LeetHub

--- 1055-pairs-of-songs-with-total-durations-divisible-by-60/1055-pairs-of-songs-with-total-durations-divisible-by-60.py
+++ 1055-pairs-of-songs-with-total-durations-divisible-by-60/1055-pairs-of-songs-with-total-durations-divisible-by-60.py
@@ -3,15 +3,6 @@ class Solution:
         hmap = {}
         result = 0
 
-        # for i in range(0,len(time)):
-        #     hmap[i] = time[i]
-        # print(hmap)
-        
-        # for i in range(0,len(time)):
-        #     if (time[i] * 60) in hmap.values():
-        #         result += 1
-        #         hmap.remove(i)
-        # return result
         for i in range(0,len(time)):
             remainder,complement = time[i]%60, -time[i]%60
             if remainder in hmap.keys():
@@ -21,10 +12,4 @@ class Solution:
 
 
 
-        # result = i = 0 
-        # for i in range(0,len(time)-1):
-            
-        #     for j in range(i+1,len(time)):
-        #         if (time[i]+time[j])%60 == 0:
-        #             result+=1
-        # return result
+     
