8aa213168c40175d244fca46b47bdfab4cbcfc63
Lemuel Addy <Lemuel.addy@gmail.com>
2023-10-02 15:38:56 +0000

    Time: 130 ms (57%), Space: 16.8 MB (31.76%) - LeetHub

--- 0696-count-binary-substrings/0696-count-binary-substrings.py
+++ 0696-count-binary-substrings/0696-count-binary-substrings.py
@@ -1,15 +1,27 @@
 class Solution:
     def countBinarySubstrings(self, s: str) -> int:
-        q = deque()
-        ans = 0
-        for i in s:
-            while q and q[-1]==i and q[0]!=i: 
-                q.pop()
-            if q and q[-1]!=i:
-                q.pop()
-                ans+=1
-            q.appendleft(i)
-        return ans
+        #Using Queue
+        # q = deque()
+        # ans = 0
+        # for i in s:
+        #     while q and q[-1]==i and q[0]!=i: 
+        #         q.pop()
+        #     if q and q[-1]!=i:
+        #         q.pop()
+        #         ans+=1
+        #     q.appendleft(i)
+        # return ans
 
-            
+        #Faster Runtime with less Space
+        ans = 0
+        cur,prev = 1,0
+        for i in range(1,len(s)):
+            if s[i]!=s[i-1]:
+                ans += min(cur,prev)
+                prev = cur
+                cur = 1
+            else:
+                cur+=1
+        return ans+min(cur,prev)
+
             
