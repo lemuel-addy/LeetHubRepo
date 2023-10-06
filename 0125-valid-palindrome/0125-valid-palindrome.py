14d4122661f75be5ec018515017084d27d4ac955
Lemuel Addy <Lemuel.addy@gmail.com>
2023-10-06 18:52:28 +0000

    Time: 53 ms (43.63%), Space: 17.1 MB (55.61%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,50 +1,18 @@
 class Solution:
-    def alphaNum(self,c):
-            return (ord('A')<= ord(c)<= ord('Z') or ord('a')<= ord(c)<= ord('z') or ord('0')<= ord(c)<= ord('9'))
-
     def isPalindrome(self, s: str) -> bool:
-        # strs = ""
-        # for i in range(0, len(s)):
-        #     if s[i].isalnum():
-        #         strs = strs + s[i].lower()      
-        # for c in range(0,len(strs)):
-        #     if strs[c] != strs[-1-c]:
-        #         return False   
-
-        # return True
-
-        #uses alphanumeric function as well as more memory
-        # strs = []
-        # for i in s:
-        #     if i.isalnum():
-        #         strs.append(i.lower())
-        # for h in range(0,len(strs)):
-        #     if strs[h] != strs[-1-h]:
-        #         return False
-        # return True
-
-        # return strs == strs[::-1]
-
-
-        #using constant extra memory using pointers
-        first = 0
-        last = len(s)-1
-        while first < last:
-            if not self.alphaNum(s[first]):
-                first += 1
+        i = 0
+        j = len(s)-1
+        while i<j:
+            if not s[i].isalnum():
+                i+=1
                 continue
-            if not self.alphaNum(s[last]):
-                last -= 1
+            if not s[j].isalnum():
+                j-=1
                 continue
-            if s[first].lower() != s[last].lower():
+            if s[i].lower() != s[j].lower():
                 return False
-            first += 1
-            last -= 1
+            j-=1
+            i+=1
         return True
 
-
-        # without using alphanumeric function
-
         
-             
-                
