56ff56872383371c384caba61a1fbaab8caed0f9
Lemuel Addy <Lemuel.addy@gmail.com>
2023-10-06 08:29:39 +0000

    Time: 39 ms (74.95%), Space: 16.2 MB (78%) - LeetHub

---
+++ 2406-decode-the-message/2406-decode-the-message.py
@@ -0,0 +1,16 @@
+class Solution:
+
+    def decodeMessage(self, key: str, message: str) -> str:
+        d={' ':' '}
+        c=97
+        for i in range(len(key)):
+            if key[i] not in d:
+                d[key[i]]=chr(c)
+                c+=1
+
+        result=''    
+        for i in range(len(message)):
+
+            result+=d[message[i]]  
+        return result        
+        
