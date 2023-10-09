91cbc9f844cfc3135dc6eda017f4c60e448320f2
Lemuel Addy <Lemuel.addy@gmail.com>
2023-10-09 22:04:05 +0000

    Time: 276 ms (12.81%), Space: 20.7 MB (36.77%) - LeetHub

--- 1055-pairs-of-songs-with-total-durations-divisible-by-60/1055-pairs-of-songs-with-total-durations-divisible-by-60.py
+++ 1055-pairs-of-songs-with-total-durations-divisible-by-60/1055-pairs-of-songs-with-total-durations-divisible-by-60.py
@@ -1,14 +1,26 @@
 class Solution:
     def numPairsDivisibleBy60(self, time: List[int]) -> int:
         hmap = {}
-        result = 0
+        ans = 0
 
         for i in range(0,len(time)):
-            remainder,complement = time[i]%60, -time[i]%60
-            if remainder in hmap.keys():
-                result+=hmap[remainder]
-            hmap[complement] = hmap.get(complement,0)+1
-        return result
+            rem,com = time[i]%60, -time[i]%60
+            if com in hmap.keys():
+                ans+=hmap[com]
+            hmap[rem] = hmap.get(rem,0)+1
+        return ans
+
+
+
+        # hmap = {}
+        # result = 0
+
+        # for i in range(0,len(time)):
+        #     remainder,complement = time[i]%60, -time[i]%60
+        #     if remainder in hmap.keys():
+        #         result+=hmap[remainder]
+        #     hmap[complement] = hmap.get(complement,0)+1
+        # return result
 
 
 
