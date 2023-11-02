42cbeac8849d7ac18684e567d3c503dff10eabe7
Lemuel Addy <Lemuel.addy@gmail.com>
2023-11-02 16:04:48 +0000

    Time: 255 ms (34.61%), Space: 18.9 MB (79.35%) - LeetHub

--- 1582-design-browser-history/1582-design-browser-history.py
+++ 1582-design-browser-history/1582-design-browser-history.py
@@ -4,25 +4,25 @@ class ListNode:
         self.prev = prev
         self.next = next 
 
-
+# LinkedList solution
 class BrowserHistory:
 
     def __init__(self, homepage: str):
         self.cur = ListNode(homepage)
         
-
+    #O(1) runtime 
     def visit(self, url: str) -> None:
         self.cur.next = ListNode(url,self.cur)
         self.cur = self.cur.next
         
-
+    #O(N) runtime
     def back(self, steps: int) -> str:
         while self.cur.prev and steps>0:
             self.cur = self.cur.prev
             steps -= 1
         return self.cur.val
         
-
+    #O(N) runtime
     def forward(self, steps: int) -> str:
         while self.cur.next and steps>0:
             self.cur = self.cur.next
