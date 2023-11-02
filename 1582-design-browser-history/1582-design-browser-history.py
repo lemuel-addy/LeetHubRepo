5862c574148596e5a4c5f4d13be54a31dd47206a
Lemuel Addy <Lemuel.addy@gmail.com>
2023-11-02 16:14:29 +0000

    Time: 199 ms (82.35%), Space: 18.9 MB (79.35%) - LeetHub

--- 1582-design-browser-history/1582-design-browser-history.py
+++ 1582-design-browser-history/1582-design-browser-history.py
@@ -4,30 +4,79 @@ class ListNode:
         self.prev = prev
         self.next = next 
 
-# LinkedList solution
+# Stack/Array solution
 class BrowserHistory:
-
     def __init__(self, homepage: str):
-        self.cur = ListNode(homepage)
-        
-    #O(1) runtime 
+        self.i = 0
+        self.len = 1
+        self.history=[homepage]
+
     def visit(self, url: str) -> None:
-        self.cur.next = ListNode(url,self.cur)
-        self.cur = self.cur.next
-        
-    #O(N) runtime
+        #check if the history length is smaller than the position where the visit should be added
+        #two because next is +1 and len is +1
+        if len(self.history) < self.i+2:
+            self.history.append(url)
+        #else set the visit url to the next position of self in history, cutting of what was there
+        else:
+            self.history[self.i+1]=url
+        #update self to point to the new position added 
+        self.i += 1
+        #reset the length 
+        self.len = self.i+1
+
     def back(self, steps: int) -> str:
-        while self.cur.prev and steps>0:
-            self.cur = self.cur.prev
-            steps -= 1
-        return self.cur.val
-        
-    #O(N) runtime
+        #subtract the steps from the current position, checking for edge case
+        self.i = max(self.i-steps,0)
+        #return that url position in history 
+        return self.history[self.i]
+
     def forward(self, steps: int) -> str:
-        while self.cur.next and steps>0:
-            self.cur = self.cur.next
-            steps -= 1
-        return self.cur.val
+        #add the steps to the current position, using len to check for edge case
+        self.i = min(self.len-1,self.i+steps)
+        #return that url position in history 
+        return self.history[self.i]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # def __init__(self, homepage: str):
+    #     self.cur = ListNode(homepage)
+        
+    # #O(1) runtime 
+    # def visit(self, url: str) -> None:
+    #     self.cur.next = ListNode(url,self.cur)
+    #     self.cur = self.cur.next
+        
+    # #O(N) runtime
+    # def back(self, steps: int) -> str:
+    #     while self.cur.prev and steps>0:
+    #         self.cur = self.cur.prev
+    #         steps -= 1
+    #     return self.cur.val
+        
+    # #O(N) runtime
+    # def forward(self, steps: int) -> str:
+    #     while self.cur.next and steps>0:
+    #         self.cur = self.cur.next
+    #         steps -= 1
+    #     return self.cur.val
         
 
 
